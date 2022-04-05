52ec974c81ffc675ffbab04d8efd8da35a7f8594
Itai Gall <itai.gall972@gmail.com>
2022-04-05 19:21:47 +0300

    wsgi file configured

--- GoldfishMemory_Project/wsgi.py
+++ GoldfishMemory_Project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GoldfishMemory_ProjectDirectory project.
+WSGI config for GoldfishMemory project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'venv.GoldfishMemory_ProjectDirectory.GoldfishMemory_Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoldfishMemory_Project.settings')
 
 application = get_wsgi_application()
