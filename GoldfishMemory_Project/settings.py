028c41af9a2bae1127704d97039278ecc213cbdd
Itai Gall <itai.gall972@gmail.com>
2022-04-05 19:56:33 +0300

    django-on-heroku configured

--- GoldfishMemory_Project/settings.py
+++ GoldfishMemory_Project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,5 +141,5 @@ LOGOUT_REDIRECT_URL = 'homepage'
 LOGIN_REDIRECT_URL = 'homepage'
 
 # Configure Django App for Heroku.
-import django_on_heroku
+#import at the top
 django_on_heroku.settings(locals())
